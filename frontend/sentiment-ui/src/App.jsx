b73ebb0b875a57fb01881e4f09c70931c69d90ee
Lucas-2306 <lucas.ferreira2306@gmail.com>
2026-03-28 03:41:23 -0300

    Changing back backend call

--- frontend/sentiment-ui/src/App.jsx
+++ frontend/sentiment-ui/src/App.jsx
@@ -13,7 +13,7 @@ import {
 
 import "./index.css";
 
-const API_URL = "http://127.0.0.1:8000"; // change to Render when deploying
+const API_URL = "https://analise-de-sentimento-automatica-asa.onrender.com"; // change to Render when deploying
 
 const COLORS = ["#22c55e", "#9ca3af", "#ef4444"];
 
